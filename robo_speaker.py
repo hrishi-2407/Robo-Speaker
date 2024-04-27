c0e525c6639cb4450f0e45eba252d36793f55f7d
Hrishi <116083480+hrishi-2407@users.noreply.github.com>
2024-04-27 08:17:39 +0530

    Add files via upload

---
+++ robo_speaker.py
@@ -0,0 +1,18 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    print('==== Welcome to Robo Speaker ====')
+    engine = pyttsx3.init()
+    while True:
+        try:
+            x = input('Write what you want me to speak: ')
+            if x == 'q':
+                engine.say('Bye, it was nice talking to you')
+                engine.runAndWait()
+                break
+            engine.say(x)
+            engine.runAndWait()
+        except:
+            engine.say('Goodbye')
+            engine.runAndWait()
+            break
